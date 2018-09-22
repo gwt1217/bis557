eebece21394d3b95e8e29433b60c661c111e1f15
wenting gao <wenting.gao@yale.edu>
2018-09-22 15:12:03 -0400

    test

--- R/linear-models.r
+++ R/linear-models.r
@@ -1,14 +1,21 @@
 
 #' Fit a linear model
 #'
-#' @description This function passes parameters to the lm function.
+#' @description This function passes arguments to the lm function.
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @examples
-#' fit <- linear_model(Sepal.Length ~., iris)
-#' summary(fit)
-#' @export
+#' @importFrom stats model.matrix 
+#' @export 
 linear_model <- function(formula, data) {
-  # Your code here.
+  a<- all.vars(formula)
+  #create response varible y
+  y <- data[,a[1]]
+  #create matrix containing coefficients and intercept
+  matrix <- model.matrix(formula, data)
+  output <- list()
+  output$coefficients <- qr.coef(qr(matrix),y)
+  #create lm object
+  class (output) = "lm"
+  return(output)
 }
